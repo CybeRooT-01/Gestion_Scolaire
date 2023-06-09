5a956bd3f85c12b3226b45ac550ea079d5843a99
CybeRooT-01 <birahim267@gmail.com>
2023-06-09 02:58:50 +0000

    drop training <li>

--- Views/cycles/elementaire.php
+++ Views/cycles/elementaire.php
@@ -1,31 +1,32 @@
 <div class="container">
     <h1>Liste des classes primaires</h1>
     <div class="container">
-    <div class="row">
-        <div class="col-md-9">
-            <?php foreach ($params['active'] as $annee) : ?>
-                <a class="nav-link d-inline-block mb-2 bg-primary text-white" href="/annee">Année Scolaire: <?= $annee->annee_scolaire ?></a>
-            <?php endforeach; ?>
-        </div>
-        <div class="col-md-3 d-flex justify-content-end align-items-center">
-<a href="/classe"><i class="fa-solid fa-plus" style="color: #1a58bc;"></i></a>
+        <div class="row">
+            <div class="col-md-9">
+                <?php foreach ($params['active'] as $annee) : ?>
+                    <a class="nav-link d-inline-block mb-2 bg-primary text-white" href="/annee">Année Scolaire: <?= $annee->annee_scolaire ?></a>
+                <?php endforeach; ?>
+            </div>
+            <div class="col-md-3 d-flex justify-content-end align-items-center">
+                <a href="/classe"><i class="fa-solid fa-plus" style="color: #1a58bc;"></i></a>
+            </div>
         </div>
     </div>
-</div>
     <ul class="list-group">
-            <li class="list-group-item bg-light mb-3 text-center ">
         <?php foreach ($params['cycle'] as $primaires) : ?>
-            <a href="/liste/nom=<?= $primaires->nom ?>" class="fs-9 d-block text-dark text-decoration-none">
-                <?php foreach ($params['active'] as $annee) : ?>
-                    <?php if ($annee->annee_scolaire == $primaires->anee) : ?>
-                        <?= $primaires->nom ?>
-                    <?php endif; ?>
-            <?php endforeach; ?>
-                </a>
-        <?php endforeach; ?>
+            <?php foreach ($params['active'] as $annee) : ?>
+                <?php if ($annee->annee_scolaire == $primaires->anee) : ?>
+                    <li class="list-group-item bg-light mb-3 text-center ">
+                        <a href="/liste/nom=<?= $primaires->nom ?>" class="fs-9 d-block text-dark text-decoration-none">
+                            <?= $primaires->nom ?>
+                        </a>
+                    </li>
+                <?php endif; ?>
 
-            </li>
+            <?php endforeach; ?>
+        <?php endforeach; ?>
     </ul>
 </div>
 </body>
+
 </html>
